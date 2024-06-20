2d0a44cc2bcbb87e5c60b34729085426ddb4dd1a
papasgeorge1808 <papasgeorge1808@gmail.com>
2024-06-20 23:55:04 +0300

    внес все изменения предложенные метнором

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,8 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserServiceHy;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserServiceHy userService = new UserServiceHy();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         Util.closeConnection();
         userService.saveUser("John", "Doe", (byte) 50);
@@ -30,7 +31,6 @@ public class Main {
 
         userService.dropUsersTable();
         Util.closeConnection();
-
 
 
     }
